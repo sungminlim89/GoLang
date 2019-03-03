ea5e165fe51eaa35e40aedede2fde23d07909418
sungminlim89 <sungminlim8909@gmail.com>
2019-03-03 23:25:19 +0900

    fifth commit

---
+++ day4/gColl.go
@@ -0,0 +1,45 @@
+// garbage collection
+/**
+コンパイルする時に
+頭に GODEBUG=gctrace=1
+を付けるとgarbageCollectorの
+動きを確認できる。
+*/
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func printStats(mem runtime.MemStats) {
+	runtime.ReadMemStats(&mem)
+	fmt.Println("mem.Alloc:", mem.Alloc)
+	fmt.Println("mem.TotalAlloc:", mem.TotalAlloc)
+	fmt.Println("mem.HeapAlloc:", mem.HeapAlloc)
+	fmt.Println("mem.NumGC:", mem.NumGC)
+	fmt.Println("-----")
+}
+
+func main() {
+	var mem runtime.MemStats
+	printStats(mem)
+
+	for i := 0; i < 10; i++ {
+		s := make([]byte, 50000000)
+		if s == nil {
+			fmt.Println("Operation failed!")
+		}
+	}
+	printStats(mem)
+
+	for i := 0; i < 10; i++ {
+		s := make([]byte, 100000000)
+		if s == nil {
+			fmt.Println("Operation failed!")
+		}
+		time.Sleep(5 * time.Second)
+	}
+	printStats(mem)
+}
